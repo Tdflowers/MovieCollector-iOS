30bc057598c28a3bfd05e80465bf1d44a6444c25
Tyler Flowers <tylerflowers@me.com>
2020-12-22 16:47:22 -0500

    Initial Commit

---
+++ MovieCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieCollector
+//
+//  Created by Tyler Flowers on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
